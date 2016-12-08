c1f8e048dc48279bbbefa4ca5162db317c8441c3
Joe Lucero <jlucerond@gmail.com>
2016-12-08 16:58:19 -0500

    Add a AddItemVC
    Initial commit. Added a very basic VC w/ navigation controller embedded
    in.

---
+++ AddItemViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddItemViewController.swift
+//  Checklists3
+//
+//  Created by Joe Lucero on 12/8/16.
+//  Copyright © 2016 Joe Lucero. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddItemViewController: UITableViewController {
+    
+    @IBAction func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func done() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
